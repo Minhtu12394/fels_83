b4b567d9d5ae8be2d90dea42af930dc961e17a81
Nguyen Tien Manh <manh_nt@hotmail.com>
2015-12-09 16:01:08 +0700

    Make API for Signin - Signup

--- db/migrate/20151027030519_create_users.rb
+++ db/migrate/20151027030519_create_users.rb
@@ -7,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.string :remember_digest, null: true
       t.string :activation_digest, null: true
-      t.boolean :activated, default: false
+      t.boolean :activated, default: true
       t.datetime :activated_at, null: true
       t.string :reset_digest, null: true
       t.datetime :reset_send_at, null: true
